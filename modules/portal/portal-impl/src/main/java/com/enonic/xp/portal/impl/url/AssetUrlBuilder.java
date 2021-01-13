80a7c671a6e561fb07e78c2404d13199c6a34b36
rymsha <rymsha@yandex.ru>
2021-01-13 12:39:33 +0100

    Per-asset fingerprint #8612 PoC

--- modules/portal/portal-impl/src/main/java/com/enonic/xp/portal/impl/url/AssetUrlBuilder.java
+++ modules/portal/portal-impl/src/main/java/com/enonic/xp/portal/impl/url/AssetUrlBuilder.java
@@ -2,13 +2,18 @@ package com.enonic.xp.portal.impl.url;
 
 import com.google.common.collect.Multimap;
 
-import com.enonic.xp.app.Application;
 import com.enonic.xp.app.ApplicationKey;
+import com.enonic.xp.core.internal.security.MessageDigests;
 import com.enonic.xp.portal.url.AssetUrlParams;
+import com.enonic.xp.resource.ResourceKey;
+import com.enonic.xp.resource.ResourceProcessor;
+import com.enonic.xp.util.HexEncoder;
 
 final class AssetUrlBuilder
     extends GenericEndpointUrlBuilder<AssetUrlParams>
 {
+    private static final String ROOT_ASSET_PREFIX = "assets/";
+
     public AssetUrlBuilder()
     {
         super( "asset" );
@@ -19,26 +24,43 @@ final class AssetUrlBuilder
     {
         super.buildUrl( url, params );
 
-        Application application = resolveApplication();
-        String applicationKey = application.getKey().toString();
-        String modifiedTime = Long.toString( application.getModifiedTime().getEpochSecond() );
-        appendPart( url, applicationKey + ":" + modifiedTime );
+        ApplicationKey application = resolveApplication();
+        String applicationKey = application.toString();
+        final ResourceKey resourceKey;
+        if ( this.params.getPath() == null )
+        {
+            resourceKey = ResourceKey.from( application, "META-INF/MANIFEST.MF" );
+        }
+        else
+        {
+            resourceKey = ResourceKey.from( application, ROOT_ASSET_PREFIX + this.params.getPath() );
+        }
+        final String hash = this.resourceService.processResource( createProcessor( resourceKey ) );
+
+        if ( hash == null )
+        {
+            throw new IllegalArgumentException( "Could not find asset [" + resourceKey + "]" );
+        }
+        appendPart( url, applicationKey + ":" + hash );
 
         appendPart( url, this.params.getPath() );
     }
 
-    private Application resolveApplication()
+    private ApplicationKey resolveApplication()
     {
-        final ApplicationKey applicationKey = new ApplicationResolver().
+        return new ApplicationResolver().
             portalRequest( this.portalRequest ).
             application( this.params.getApplication() ).
             resolve();
+    }
 
-        final Application application = this.applicationService.getInstalledApplication( applicationKey );
-        if ( application == null )
-        {
-            throw new IllegalArgumentException( "Could not find application [" + applicationKey + "]" );
-        }
-        return application;
+    private ResourceProcessor<ResourceKey, String> createProcessor( final ResourceKey key )
+    {
+        return new ResourceProcessor.Builder<ResourceKey, String>().
+            key( key ).
+            segment( "assetDescriptor" ).
+            keyTranslator( rk -> rk ).
+            processor( resource -> HexEncoder.toHex( MessageDigests.sha512().digest( resource.readBytes() ) ) ).
+            build();
     }
 }
